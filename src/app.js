59ac615f15bb78d5236c23ef6a49eb1d693a0c37
psoto-go <pabletesglez@gmail.com>
2021-05-19 17:43:00 +0000

    Fin Excuse Generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+let who = ["the dog", "my granma", "his turtle", "my bird"];
+let what = ["eat", "pissed", "crushed", "broked"];
+let what_1 = ["my homework", "my phone", "my book"];
+let when = [
+  "before the class",
+  "right in time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+function get_num(length) {
+  return Math.floor(Math.random() * length);
+}
+var a = (window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+  var excuse =
+    who[get_num(who.length)] +
+    " " +
+    what[get_num(what.length)] +
+    " " +
+    what_1[get_num(what_1.length)] +
+    " " +
+    when[get_num(when.length)];
+  document.getElementById("excuse").innerHTML = excuse;
+});
